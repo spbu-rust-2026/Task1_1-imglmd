0d792c81c47daa8383d7ec818f3d9a4fa60650de
kir <dangdatmini@gmail.com>
2026-09-18 17:08:18 +0300

    Complete homework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("error");
+
+    let mut iter = input.split_whitespace();
+    let a: i64 = iter.next().unwrap().parse().unwrap();
+    let b: i64 = iter.next().unwrap().parse().unwrap();
+
+    println!("{}", a + b);
 }
